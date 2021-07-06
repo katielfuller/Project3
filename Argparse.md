d809fd9d2da2bf30edfc2cfb7d6212f62f9ff694
katielfuller <katielfuller01@gmail.com>
2021-07-06 14:33:17 -0500

    working on choices

--- Argparse.md
+++ Argparse.md
@@ -2,5 +2,5 @@ In the Argparse method I want the user to be able to
 1. input cases followed by a county or Texas, which will return the number of confirmed cases for that county
 2. input deaths followed by a county or Texas, which will return the number of deaths for that county
 3. input list, which will return a list of counties 
-4. input help, which will return a list of the commands the user can imput 
+4. input help, which will return a list of the commands the user can input 
 5. create an error message if the user doesn't correctly enter one of the first 4 commands
--- argparse_michael.py
+++ argparse_michael.py
@@ -1,6 +1,40 @@
+import argparse
+import sys
+
 def formatter(prog):
     return argparse.HelpFormatter(prog, max_help_position=100, width=200)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description = "This shows current Covid cases and Covid deaths in Texas and Texas counties")
 
+
+
+    parser.add_argument('county-covid-data.txt', type=argparse.FileType('r'))
+    args = parser.parse_args()
+    with open(args.county-covid-data.txt) as file:
+
+
+
+    # The arpgparse module makes it easy to write user-friendly command-line interfaces.  The argparse module
+    # also automatically generates help and usage messages and issues errors when users give the program invalid arguments
+    # e.g. usingArgParse -h
+    #
+    parser = argparse.ArgumentParser(description='This script shows current COVID infection and death rates for all Texas counties', formatter_class=formatter)
+    #parser = argparse.ArgumentParser(description='This script shows current COVID infection and death rates for all Texas counties')
+    # Specify the three possible options.  Must specify exactly one of them
+    parser.add_argument('-c', '--Cases', help='Covid cases in the specified county', type=str, choices=[county-covid-data.txt])
+    parser.add_argument('-d', '--Deaths', help='Covid deaths in the specified county', type=str, choices=[county-covid-data.txt])
+    parser.add_argument('-cnty', '--County', help='The Texas county of interest (defaults to statewide)', default='statewide', type=str)
+    parser.add_argument('-l', '--List', help='The list of counties in Texas', type=str)
+    args = parser.parse_args(args=None if sys.argv[1:] else ['--help'])
+
+    print(f'Reporting for {args.County}')
+
+    if args.Cases:
+        print(f'You selected Counties.  The value of args.Counties is {args.County}')
+
+    if args.Deaths:
+        print(f'You selected Cases.  The value of args.Cases is {args.County}')
+
+    if args.List:
+        print(f'when i get this i will print the list of counties - {args.List}')
